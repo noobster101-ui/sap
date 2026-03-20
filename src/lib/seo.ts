2de78ed51cb12ece13c8ab9d6ff9b3bc731c6704
Rakesh <rakesh.sing@bynaric.in>
2026-03-20 18:32:11 +0530

    update sitemap

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -31,7 +31,7 @@ export function createPageMetadata({
     title,
     description,
     alternates: {
-      canonical: "./",
+      canonical: "/",
     },
     openGraph: {
       ...baseMetadata.openGraph,
